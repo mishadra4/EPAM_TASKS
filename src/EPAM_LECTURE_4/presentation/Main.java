ce992fbab8138629ad41a99a45faca11ec55e43b
Mykhajlo Drach <qwewerty12@gmail.com>
2018-01-14 22:42:54 +0200

    Third hometask refactoring

---
+++ src/EPAM_LECTURE_4/presentation/Main.java
@@ -0,0 +1,16 @@
+package EPAM_LECTURE_4.presentation;
+
+import java.time.LocalDate;
+import java.time.chrono.Chronology;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale ukraine = Locale.getDefault();
+        ResourceBundle phrases = ResourceBundle.getBundle("Phrases", ukraine);
+        System.out.println("Current locale is: " + ukraine.getDisplayName());
+        System.out.println(phrases.getString("hello"));
+        System.out.println(phrases.getString("thanks"));
+    }
+}
